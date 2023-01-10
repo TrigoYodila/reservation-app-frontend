cfdd5b9de0c3fda71ffaba6d996d7b79297b5038
Trigo YODILA <trigoyodila1996@gmail.com>
2023-01-10 12:34:17 +0100

    Feat: add login page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { SearchContextProvider } from "./context/SearchContext.js";
-import { AuthContextProvider } from "./context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
